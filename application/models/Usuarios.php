46803a357b1bb43346d5f1192d5e4fdad33cd4d9
Juan Araujo <juane.araujob@gmail.com>
2018-09-27 16:04:59 -0500

    Validacion de contraseña encriptada para administrador y administrativo

--- application/models/Usuarios.php
+++ application/models/Usuarios.php
@@ -85,30 +85,40 @@ class Usuarios extends CI_Model{
                 else{
                     $array_out = array("return"=>"failure");
                 }
+            }else{
+                $array_out = array("return"=>"failure");
             }
            
         }
 
     	else if( $tipo == 'Administrador'){ //verificar usuario, pass y de perfil, si existe devolver modulos asignados
-             $query = $this->db->query("
+            $query = $this->db->query("
+            SELECT verificar_password('".$user."','".$pass."')"
+            );
+            $data = $query->result_array();
+            if($data != NULL)
+            {
+                $query = $this->db->query("
                 SELECT u.user_name as nombre, u.pass, u.id_usuario as id FROM usuario as u
                 INNER JOIN usuario_perfil p on (u.id_usuario = p.id_usuario)
                 INNER JOIN perfil i on (i.id_perfil = p.id_perfil)
-                WHERE i.id_perfil = 1 AND lower(u.user_name) = '".$user."' AND u.pass = '".$pass."'"
-            );
+                WHERE i.id_perfil = 1 AND u.id_usuario= '".$data[0]['id']."'"
+                );
 
+                $data = $query->result_array();
 
-            $data = $query->result_array();
-
-            if(count($data) == 1){
-                $array_out = array("return"=>"success","id"=>$data[0]['id'],"user"=>$data[0]['nombre']);
-                $query = $this->db->query("
-                    SELECT id_mod as modulos FROM public.usuario_modulo
-                    WHERE id_usuario = ".$data[0]['id']." ORDER BY id_mod;
-                ");
-                $array_out['modulos'] = $query->result_array();
-            }
-            else{
+                if(count($data) == 1){
+                    $array_out = array("return"=>"success","id"=>$data[0]['id'],"user"=>$data[0]['nombre']);
+                    $query = $this->db->query("
+                        SELECT id_mod as modulos FROM public.usuario_modulo
+                        WHERE id_usuario = ".$data[0]['id']." ORDER BY id_mod;
+                    ");
+                    $array_out['modulos'] = $query->result_array();
+                }
+                else{
+                    $array_out = array("return"=>"failure");
+                }
+            }else{
                 $array_out = array("return"=>"failure");
             }
     	}
