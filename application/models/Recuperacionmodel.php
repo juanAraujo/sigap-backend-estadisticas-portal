7e6da681b2221d32e8b3043f467814cedcda62fd
Juan Araujo <juane.araujob@gmail.com>
2018-11-16 02:06:48 -0500

    arreglando querys

--- application/models/Recuperacionmodel.php
+++ application/models/Recuperacionmodel.php
@@ -6,7 +6,11 @@ class Recuperacionmodel extends CI_Model
 		parent::__construct();
 	}
 	public function comprobar_existencia($email,$dni,$telefono){
-		$query=$this->db->query("select a.id_usuario from usuario u,administrativo a,usuario_perfil up where a.email = ".$email." and a.dni=".$dni." and a.telefono=".$telefono." and a.id_usuario = up.id_usuario and up.id_usuario = u.id_usuario and (up.id_perfil = 1 or up.id_perfil = 2) and up.estado_up=true;");
+		$query=$this->db->query
+		("select ad.id_usuario from usuario u INNER JOIN administrativo ad ON u.id_usuario=ad.id_usuario 
+		INNER JOIN usuario_perfil up ON up.id_usuario=u.id_usuario
+		where ad.email = '".$email."' and ad.dni='".$dni."' and ad.telefono='".$telefono."' 
+		and (up.id_perfil = 1 or up.id_perfil = 2) and up.estado_up=true;");
 		//print_r($query);
 		$data=$query->result_array();
 		//echo "sdfsdf".count($data);
@@ -18,7 +22,7 @@ class Recuperacionmodel extends CI_Model
 			
 	}
 	public function actualizar_pass($id,$nuevapass){
-		$this->db->query("update usuario set pass = ".$nuevapass." where id_usuario= (select id_usuario from administrativo where id_admin= ".$id.")");
+		$this->db->query("update usuario set pass = '".$nuevapass."' where id_usuario= '".$id."'");
 		if ($this->db->affected_rows() > 0) {   
 			return TRUE; 
 		} else {   
