f9f7d53cbc9b34853dcdc95576809686fbf0eda8
Juan Araujo <juane.araujob@gmail.com>
2018-11-30 18:44:15 -0500

    Cambio

---
+++ application/models/CambioMoneda.php
@@ -0,0 +1,34 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+class CambioMoneda extends CI_Model
+{
+    function __construct(){
+		parent::__construct();
+    }
+    public function cambiarASoles($fecha)
+    {
+        $ruc = $_POST['ruc'];
+        $data = file_get_contents("https://api.sunat.cloud/ruc/".$ruc);
+        $info = json_decode($data, true);
+        echo ($fecha);
+        if($data==='[]' || $info['fecha_inscripcion']==='--'){
+            $datos = array(0 => 'nada');
+            echo json_encode($datos);
+        }else{
+        $datos = array(
+            0 => $info['ruc'], 
+            1 => $info['razon_social'],
+            2 => date("d/m/Y", strtotime($info['fecha_actividad'])),
+            3 => $info['contribuyente_condicion'],
+            4 => $info['contribuyente_tipo'],
+            5 => $info['contribuyente_estado'],
+            6 => date("d/m/Y", strtotime($info['fecha_inscripcion'])),
+            7 => $info['domicilio_fiscal'],
+            8 => date("d/m/Y", strtotime($info['emision_electronica']))
+        );
+            echo json_encode($datos);
+        }
+    }
+}
+?>
