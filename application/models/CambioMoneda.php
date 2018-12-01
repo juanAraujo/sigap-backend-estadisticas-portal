e045ce60e4196eb317f72723d0bd7f26b3780f64
Juan Araujo <juane.araujob@gmail.com>
2018-11-30 20:11:31 -0500

    Probando

--- application/models/CambioMoneda.php
+++ application/models/CambioMoneda.php
@@ -20,23 +20,20 @@ class CambioMoneda extends CI_Model
         $info = json_decode($data, true);
         $array_out = $data;
         echo ($array_out);
+        echo ($info);
+
         if($data==='[]' || $info['fecha_inscripcion']==='--'){
             $datos = array(0 => 'nada');
             echo json_encode($datos);
         }else{
-        $datos = array(
-            0 => $info['ruc'], 
-            1 => $info['razon_social'],
-            2 => date("d/m/Y", strtotime($info['fecha_actividad'])),
-            3 => $info['contribuyente_condicion'],
-            4 => $info['contribuyente_tipo'],
-            5 => $info['contribuyente_estado'],
-            6 => date("d/m/Y", strtotime($info['fecha_inscripcion'])),
-            7 => $info['domicilio_fiscal'],
-            8 => date("d/m/Y", strtotime($info['emision_electronica']))
-            );
-            echo json_encode($datos);
+            $array_out = array();
+            if(count($data)>0){
+                foreach ($data as $registro) {
+                    $array_out[] = $registro;
+                }
+            }
         }
+        echo ($array_out);
         return $array_out;
     }
 
