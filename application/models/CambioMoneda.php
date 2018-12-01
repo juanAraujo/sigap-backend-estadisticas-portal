48f9ee180e655ea71e200f046e1c6ad43d1e7fd6
Juan Araujo <juane.araujob@gmail.com>
2018-11-30 20:46:21 -0500

    Probando

--- application/models/CambioMoneda.php
+++ application/models/CambioMoneda.php
@@ -9,29 +9,37 @@ class CambioMoneda extends CI_Model
     
     public function cambiarASoles($fecha)
     {
-        $ch = curl_init("https://api.sunat.cloud/cambio/".$fecha);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        $data = curl_exec($ch);
-        curl_close($ch);
+        do{
+            $ch = curl_init("https://api.sunat.cloud/cambio/".$fecha);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+            //resto 1 día
+            $fecha = date("Y-m-d",strtotime($fecha."- 1 days")); 
+             
+            echo $fecha;
+
+        }while($info === '[]');
+
+        
 
         $info = json_decode($data, true);
         $array_out = $data;
         echo ($array_out);
-        echo ($info[0]);
 
         if($data==='[]' || $info['fecha_inscripcion']==='--'){
             $datos = array(0 => 'nada');
             echo json_encode($datos);
         }else{
-            $array_out = array();
-            if(count($data)>0){
-                foreach ($data as $registro) {
-                    $array_out[] = $registro;
-                }
-            }
+            $array_out = array(
+            0 => $info['compra'], 
+            1 => $info['venta'],
+            );
+            echo json_encode($datos);
         }
         echo ($array_out[0]);
         return $array_out;
