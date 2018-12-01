c256e6251307463dd4c3ec87649e41f5b6659271
Juan Araujo <juane.araujob@gmail.com>
2018-11-30 19:26:09 -0500

    Probando 11

--- application/models/CambioMoneda.php
+++ application/models/CambioMoneda.php
@@ -8,7 +8,7 @@ class CambioMoneda extends CI_Model
     }
     public function cambiarASoles($fecha)
     {
-        $data = file_get_contents("https://api.sunat.cloud/".urlencode($fecha));
+        $data = curl_get_contents("https://api.sunat.cloud/".urlencode($fecha));
         $info = json_decode($data, true);
         $array_out = $data;
         echo ($fecha);
@@ -30,6 +30,18 @@ class CambioMoneda extends CI_Model
             echo json_encode($datos);
         }
         return $array_out;
+    }
+
+    function curl_get_contents($url)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
     }
 }
 ?>
